da6434c4e37e9f5b1399b9801041a3d16ce7ed1a
Gorun <307532500@qq.com>
2024-03-25 21:36:08 +0800

    测试IP表

--- src/des.cpp
+++ src/des.cpp
@@ -14,8 +14,11 @@ std::bitset<64> des::ip(const std::bitset<64>& rhs) {
     return result;
 }
 
-std::bitset<64> ip_r(const std::bitset<64>& rhs) {
+std::bitset<64> des::ip_r(const std::bitset<64>& rhs) {
     std::bitset<64> result;
-    
+    for (int offset = 0; offset < 64; offset++) {
+        int target_index = ip_rt[offset] - 1;
+        result[offset] = rhs[target_index];
+    }
     return result;
 }
--- test/test_add_lib.cpp
+++
@@ -1,6 +0,0 @@
-#include "des.h"
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("AddTest", "[add]") {
-    REQUIRE(des::add(1, 2) == 3);
-}
